f63da3cc68070cd7db3dbb20f815155fbca1e244
muiida <flashingwind@gmail.com>
2024-11-10 19:47:31 +0900

    ft_strjoin: NULLチェックがうまくいってなかった

--- ft_additonal_string2.c
+++ ft_additonal_string2.c
@@ -6,7 +6,7 @@
 /*   By: muiida <muiida@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 17:13:11 by muiida            #+#    #+#             */
-/*   Updated: 2024/10/31 00:56:20 by muiida           ###   ########.fr       */
+/*   Updated: 2024/11/10 19:44:49 by muiida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,10 +65,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*dst_head;
 	size_t	len;
 
-	len = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
-	if (len == 0)
-		return (get_null_str());
-	dst = (char *)malloc(len * sizeof(char));
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	dst = (char *)malloc((len + 1) * sizeof(char));
 	if (dst == NULL)
 		return (NULL);
 	dst_head = dst;
