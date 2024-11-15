27f894cbfb23737fe4060c20c73876917482d58b
muiida <flashingwind@gmail.com>
2024-11-15 19:15:37 +0900

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,12 @@
 /*   By: muiida <muiida@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 16:29:37 by miida             #+#    #+#             */
-/*   Updated: 2024/11/14 17:50:18 by muiida           ###   ########.fr       */
+/*   Updated: 2024/11/15 19:15:22 by muiida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 static int	ft_isspace(const char c)
 {
@@ -21,7 +22,7 @@ static int	ft_isspace(const char c)
 		return (0);
 }
 
-static int	sign_to_int(const char sign)
+static long	sign_to_int(const char sign)
 {
 	if (sign == '-')
 		return (-1);
@@ -31,24 +32,57 @@ static int	sign_to_int(const char sign)
 
 int	ft_atoi(const char *s)
 {
-	long long	num;
-	int			sign;
+	long	num;
+	long	sign;
+	size_t	i;
 
-	while (ft_isspace(*s))
-		s++;
+	i = 0;
+	while (ft_isspace(s[i]))
+		i++;
 	sign = 1;
-	if (*s == '-' || *s == '+')
+	if (s[i] == '-' || s[i] == '+')
 	{
-		sign *= sign_to_int(*s);
-		s++;
+		sign *= sign_to_int(s[i]);
+		i++;
 	}
 	num = 0;
-	while (ft_isdigit(*s))
+	while (ft_isdigit(s[i]))
 	{
-		num *= 10;
-		num += *s - '0';
-		s++;
+		num = num * 10 + ((long)s[i] - (long)'0');
+		if (sign == 1 && num < 0)
+			return ((int)LONG_MAX);
+		else if (sign == -1 && num < 0)
+			return ((int)LONG_MIN);
+		i++;
 	}
 	num *= sign;
 	return (num);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// int	main(int argc, char **argv)
+// {
+// 	// char	s[] = "-2";
+// 	printf("ft: 2147483647->%d\n", ft_atoi("2147483647"));
+// 	printf("2147483647->%d\n", atoi("2147483647"));
+// 	printf("ft: 2147483648->%d\n", ft_atoi("2147483648"));
+// 	printf("2147483648->%d\n", atoi("2147483648"));
+// 	printf("ft: -2147483648->%d\n", ft_atoi("-2147483648"));
+// 	printf("-2147483648->%d\n", atoi("-2147483648"));
+// 	printf("ft: -2147483649->%d\n", ft_atoi("-2147483649"));
+// 	printf("-2147483649->%d\n", atoi("-2147483649"));
+// 	printf("ft: 9223372036854775806->%d\n", ft_atoi("9223372036854775806"));
+// 	printf("9223372036854775806->%d\n", atoi("9223372036854775806"));
+// 	printf("ft: 9223372036854775807->%d\n", ft_atoi("9223372036854775807"));
+// 	printf("9223372036854775807->%d\n", atoi("9223372036854775807"));
+// 	printf("ft: 9223372036854775808->%d\n", ft_atoi("9223372036854775808"));
+// 	printf("9223372036854775808->%d\n", atoi("9223372036854775808"));
+// 	printf("ft: -9223372036854775807->%d\n", ft_atoi("-9223372036854775807"));
+// 	printf("-9223372036854775807->%d\n", atoi("-9223372036854775807"));
+// 	printf("ft: -9223372036854775808->%d\n", ft_atoi("-9223372036854775808"));
+// 	printf("-9223372036854775808->%d\n", atoi("-9223372036854775808"));
+// 	printf("ft: -9223372036854775809->%d\n", ft_atoi("-9223372036854775809"));
+// 	printf("-9223372036854775809->%d\n", atoi("-9223372036854775809"));
+// 	return (0);
+// }
