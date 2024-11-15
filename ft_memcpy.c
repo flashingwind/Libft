1723190a9bf929d80d3e3c47d5eb4dad6ecc0f8c
muiida <flashingwind@gmail.com>
2024-11-15 20:00:27 +0900

    ft_memcpy; NULLチェックif文の書き方

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: muiida <muiida@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 21:07:53 by muiida            #+#    #+#             */
-/*   Updated: 2024/11/14 17:02:16 by muiida           ###   ########.fr       */
+/*   Updated: 2024/11/15 19:59:32 by muiida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*d;
 	unsigned char	*s;
 
-	if (!dest && !src)
+	if (dest == NULL && src == NULL)
 		return (NULL);
 	d = (unsigned char *)dest;
 	s = (unsigned char *)src;
