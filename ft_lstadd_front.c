1079b5d6271084ce2252743e93a39a7ca048ac6f
muiida <flashingwind@gmail.com>
2024-11-15 19:41:05 +0900

    ft_lstadd_front; !=NULL&&!=NULL形式からNULL|| NULLへ

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: muiida <muiida@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 01:24:50 by muiida            #+#    #+#             */
-/*   Updated: 2024/11/14 17:46:22 by muiida           ###   ########.fr       */
+/*   Updated: 2024/11/15 19:39:04 by muiida           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new_node)
 {
-	if (lst != NULL && new_node != NULL)
-	{
-		new_node->next = *lst;
-		*lst = new_node;
-	}
+	if (lst == NULL || new_node == NULL)
+		return ;
+	new_node->next = *lst;
+	*lst = new_node;
 }
